e0ca5f1aea8429ebc9fd522f637a2731a17d32d6
Amit Biswas <akbisw@gmail.com>
2017-09-26 15:21:03 -0400

    Initial Commit. Static Files

---
+++ Package.swift
@@ -0,0 +1,13 @@
+// swift-tools-version:3.1
+
+import PackageDescription
+
+let package = Package(
+    name: "imgair",
+    dependencies: [
+	.Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 7),
+	.Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 7),
+	.Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", majorVersion: 1, minor: 7),
+    .Package(url: "https://github.com/SwiftOnTheServer/SwiftDotEnv.git", majorVersion: 1, minor: 1),
+    ]
+)
---
+++ Sources/ImgAir/main.swift
@@ -0,0 +1,45 @@
+import CouchDB
+import DotEnv
+import Foundation
+import HeliumLogger
+import Kitura
+import LoggerAPI
+import SwiftyJSON
+
+// Disable buffering on log output
+setbuf(stdout, nil)
+
+// Attach a logger
+Log.logger = HeliumLogger()
+
+// Database connection. Read the .env file for db credentials
+let env = DotEnv()
+
+let connectionProperties = ConnectionProperties(
+    host: env.get("DB_HOST") ?? "localhost",
+    port: Int16(env.getAsInt("DB_PORT") ?? 5984),
+    secured: env.getAsBool("DB_HTTPS") ?? false,
+    username: env.get("DB_USERNAME") ?? "",
+    password: env.get("DB_PASSWORD") ?? ""
+)
+let databaseName = env.get("DB_NAME") ?? "image_db"
+
+let client = CouchDBClient(connectionProperties: connectionProperties)
+let database = client.database(databaseName)
+//let imagesMapper = imagesMapper(withDatabase: database)
+
+// setup routes
+let router = Router()
+router.all("/static", middleware: StaticFileServer())
+//router.get("/", handler: displayImagesHandler)
+
+//router.get("/books", handler: displayImagesHandler)
+//router.get("/books/:id", handler: getImageHandler)
+//router.post("/books", handler: insertImageHandler)
+//router.update("/books/:id", handler: updateImageHandler)
+
+// Start server
+Log.info("Starting server")
+let port = env.getAsInt("APP_PORT") ?? 8080
+Kitura.addHTTPServer(onPort: port, with: router)
+Kitura.run()
